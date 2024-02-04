1c374605fffce0405b8ca67af7cc92033eb381c5
Deepak <rohit45deepak@gmail.com>
2024-02-05 00:42:23 +0530

    done

--- main.c
+++ main.c
@@ -2,4 +2,8 @@
 void main()
 {
     printf("welocme to c programming\n");
+    printf("This is my first program\n");
+    printf("This is my first program\n");
+    printf("This is my first program\n");
+    printf("This is my first program\n");
 }
